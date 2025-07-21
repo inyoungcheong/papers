1ed3c4d5ca390a5e61ea6b23ff4b410403b5ae39
Inyoung Cheong <44386633+inyoungcheong@users.noreply.github.com>
2025-07-20 18:16:03 -0700

    Remove basePath to fix routing

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/papers',
-  assetPrefix: '/papers/',
   trailingSlash: true,
   images: {
     unoptimized: true
-  },
-  experimental: {
-    appDir: true
   }
 }
 
